ad759514dc884ce7a58b6d3244fd3d4a79a59cad
Purohit <neha.purohit3@dxc.com>
2022-09-09 10:01:45 +0530

    package of main class changed

--- src/main/java/com/example/DemoApp.java
+++ src/main/java/com/example/DemoApp.java
@@ -1,9 +1,9 @@
-package com.example.demo;
+package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class DemoApp {
 	
 	
